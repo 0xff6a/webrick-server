5f2021039ed4e95af6be2cc1c7090f4639d3e3f4
Jeremy Fox <foxjerem@gmail.com>
2014-08-29 22:05:21 +0100

    refactored

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -5,28 +5,35 @@ describe 'Posts:' do
 	context 'when there are no posts' do
 
 		it 'it should display a message on the homepage' do
-			data = Net::HTTP.get URI.parse('http://localhost:8000/posts')
+			data = _get('/posts')
 			expect(data).to include('There are no posts at present')
 		end
 
 		it 'should provide a link to new posts' do
-			data = Net::HTTP.get URI.parse('http://localhost:8000/posts')
+			data = _get('/posts')
 			expect(data).to include(_new_post_link)
 		end
 
 		it 'a user can access a form to add a new link' do
-			data = Net::HTTP.get URI.parse('http://localhost:8000/posts/new')
+			data = _get('/posts/new')
 			expect(data).to include(_new_post_form)
 		end
 
 		it 'a user can create a post from the form' do
-			params = { title: 'Test', content: '...blah...' }
-			uri = URI.parse('http://localhost:8000/posts')
-			Net::HTTP.post_form(uri, params)
-			data = Net::HTTP.get URI.parse('http://localhost:8000/posts')
+			_post('/posts', { title: 'Test', content: '...blah...' } )
+			data = _get('/posts')
 			expect(data).to include('Test') && include('...blah...')
 		end
 
+	end
+
+	def _get(route)
+		Net::HTTP.get URI.parse('http://localhost:8000' + route)
+	end
+
+	def _post(route, params)
+		uri = URI.parse('http://localhost:8000' + route)
+		Net::HTTP.post_form(uri, params)
 	end
 
 	def _new_post_link
